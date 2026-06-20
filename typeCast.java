3d1995c40128a756af57b9e4c2d8c0edf039fb56
PalashDandapat <23051201@kiit.ac.in>
2026-06-20 15:04:00 +0530

    Delete typeCast.java

--- typeCast.java
+++
@@ -1,8 +0,0 @@
-class typeCast{
-    public static void main(String args[]){
-        int a=10;
-        double b=20.10;
-        System.out.println((int)b);
-        System.out.println((double)a);
-    }
-}
